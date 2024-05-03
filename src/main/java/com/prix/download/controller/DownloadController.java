839fb9e390b13fef1804eba99d2620c21e42f902
김채은 <rlaco1005@hanyang.ac.kr>
2024-05-03 20:53:01 +0900

    init

--- src/main/java/com/prix/download/controller/DownloadController.java
+++ src/main/java/com/prix/download/controller/DownloadController.java
@@ -10,6 +10,26 @@ public class DownloadController {
         return "redirect:/download/modplus";
     }
 
+    @GetMapping("/download/actg")
+    public String actg() {
+        return "redirect:/download/actg";
+    }
+
+    @GetMapping("/download/cifter")
+    public String cifter() {
+        return "redirect:/download/cifter";
+    }
+
+    @GetMapping("/download/dbond")
+    public String dbond() {
+        return "redirect:/download/dbond";
+    }
+
+    @GetMapping("/download/demix")
+    public String deMix() {
+        return "redirect:/download/demix";
+    }
+
     @GetMapping("/download/moda")
     public String moda() {
         return "download/moda";
